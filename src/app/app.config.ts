e70cfd672958aa742dd19d24bc4887506ec035a8
Itai Barkav <itaibarkav@gmail.com>
2024-11-25 19:58:56 +0200

    Add search item input

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(BrowserAnimationsModule),
+  ],
 };
--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from '../header/header.component';
+import { MaterialModule } from '../material.module';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MaterialModule, ReactiveFormsModule],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss',
 })
 export class CatalogComponent {
+  search = new FormControl();
+
   constructor() {}
 }
