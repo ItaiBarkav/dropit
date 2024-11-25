fac95fd4c7104aa93a953ab3bb9137c81c6229c3
Itai Barkav <itaibarkav@gmail.com>
2024-11-25 17:21:36 +0200

    init catalog page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => CatalogComponent },
+  { path: '**', redirectTo: '' },
+];
