151f534c81861af57936267312b76722c8537606
Itai Barkav <itaibarkav@gmail.com>
2024-11-26 16:48:00 +0200

    Add catalog table with search

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,17 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 import { HeaderComponent } from '../header/header.component';
 import { MaterialModule } from '../material.module';
+import { ProductsService } from '../services/products.service';
+import { Product } from '../types/product';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [HeaderComponent, MaterialModule, ReactiveFormsModule],
+  imports: [HeaderComponent, MaterialModule, ReactiveFormsModule, CommonModule],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss',
 })
 export class CatalogComponent {
-  search = new FormControl();
+  searchTerm = new FormControl();
+  products: Product[] = [];
 
-  constructor() {}
+  private originProducts$ = new BehaviorSubject<Product[]>([]);
+
+  constructor(private productsService: ProductsService) {
+    this.initOriginProducts();
+    this.search();
+  }
+
+  private initOriginProducts(): void {
+    this.productsService.getProducts().subscribe((products) => {
+      this.originProducts$.next(products);
+      this.products = products;
+    });
+  }
+
+  private search(): void {
+    this.searchTerm.valueChanges.subscribe(
+      (searchTerm) =>
+        (this.products = this.originProducts$.value.filter(
+          (products) =>
+            products.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            products.id.toString().includes(searchTerm)
+        ))
+    );
+  }
 }
